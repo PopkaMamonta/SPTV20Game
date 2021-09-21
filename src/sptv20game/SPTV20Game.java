f097b6c77efef50d009b4574d2351d4cb69a1db3
Pomeloed <38099051+Pomeloed@users.noreply.github.com>
2021-09-21 14:16:50 +0300

    second

--- src/sptv20game/SPTV20Game.java
+++ src/sptv20game/SPTV20Game.java
@@ -9,19 +9,26 @@ public class SPTV20Game {
     public static void main(String[] args) {
         //Программа загадывает число и просит пользователя отгадать, 
         //если пользователь отгадал то он выйграл, иначе пользователь проиграл
-        int max=5, min=0;
+        int max=5, min=0,attempt=1;
         Random random=new Random();
-        int truenum=random.nextInt(max - min + 1) + min;
         System.out.println("Программа загадала число от 0 до 5");
+        int truenum=random.nextInt(max - min + 1) + min;
         Scanner scanner=new Scanner(System.in);
-        System.out.print("Отгадайте число: ");
-        int usernum=scanner.nextInt();
-        if(truenum==usernum){
-            System.out.println("Ура! Ты угадал!");
-        }else{
-            System.out.println(":( Ты проиграл!Загадано число: "+truenum);
+        for(int i=0;i<3;i++){
+            System.out.print("Отгадай число: ");
+            int usernum=scanner.nextInt();
+            if(truenum==usernum){
+                System.out.println("Ура! Ты угадал!");
+                break;
+            }else{
+                if(attempt<3){
+                    System.out.println("Неправильно, попробуй еще раз.");
+                }else{
+                    System.out.println(":( Ты проиграл!Загадано число: "+truenum);
+                }
+                }    
+            attempt++;
         }
-        
     }
     
 }
