29d90a8003f7f1c7813564702a1d159970e29176
anastasiia <Anastasiia.Lepova@ivkhk.ee>
2021-09-23 12:43:47 +0300

    fifth

--- src/sptv20game/SPTV20Game.java
+++ src/sptv20game/SPTV20Game.java
@@ -9,12 +9,16 @@ public class SPTV20Game {
     public static void main(String[] args) {
         //Программа загадывает число и просит пользователя отгадать, 
         //если пользователь отгадал то он выйграл, иначе пользователь проиграл
-        int max=5, min=0,attempt=1,ii=1;
+        int attempt=1,ii=1;
         Random random=new Random();
         Scanner scanner=new Scanner(System.in);
+        System.out.println("Введите с какого числа будет загадываться: ");
+        int MinDia=scanner.nextInt();
+        System.out.println("Введите до какого числа будет загадываться: ");
+        int MaxDia=scanner.nextInt();
         while(ii==1){
-        System.out.println("Программа загадала число от 0 до 5");
-        int truenum=random.nextInt(max - min + 1) + min;
+        System.out.printf("Программа загадала число от %d до %d\n",MinDia,MaxDia);
+        int truenum=random.nextInt(MaxDia - MinDia + 1) + MinDia;
         for(int i=0;i<3;i++){
             System.out.print("Отгадай число: ");
             int usernum=scanner.nextInt();
